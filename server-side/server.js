b35d9c73197599d3d952ddf9b31e955ac16d6a32
Ionii <ionitadan260399@gmail.com>
2022-09-08 01:28:36 +0300

    Update server.js

--- server-side/server.js
+++ server-side/server.js
@@ -4,7 +4,7 @@ const socketio = require('socket.io');
 
 const app = express();
 
-app.use(express.static(`${__dirname}/../public`));
+app.use(express.static(`../public`));
 
 const server = http.createServer(app);
 const io = socketio(server);
