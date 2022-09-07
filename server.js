b977ca998250040ec40f6c11faafd346b82742d9
Ionii <ionitadan260399@gmail.com>
2022-09-08 02:50:39 +0300

    Deploy on heroku #6

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 
 const server = express()
-    .use((req, res) => res.sendFile(INDEX, { root: `${__dirname}/../public` }))
+    .use((req, res) => res.sendFile(INDEX, { root: `${__dirname}/public` }))
     .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 // app.use(express.static(`${__dirname}/../public`));
